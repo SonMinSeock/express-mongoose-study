c28cffe3b0a7bde67eaea46c183a67e952b705f1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-06-16 18:13:56 +0900

    Mongo Conection Add

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// module import...
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
+// mongoose and mongoDB connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shopApp")
+  .then(() => console.log("Mongo Connection Open!!"))
+  .catch((err) => console.log("Mongo connecting Error : ", err));
+
+// define code....
 const app = express();
 const PORT = 3000;
 
@@ -8,10 +17,12 @@ const PORT = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// router
 app.get("/", (req, res) => {
   res.send("<h1>HOME</h1>");
 });
 
+// server listening
 app.listen(PORT, () => {
   console.log(`App is Listening on PORT : ${PORT}`);
 });
